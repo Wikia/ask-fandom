ad6ddbd1007fd25f938a18f30b36b46758ab37d9
macbre <maciej.brencz@gmail.com>
2019-03-08 13:40:25 +0100

    test_get_oracle: use real class names

--- tests/test_get_oracle.py
+++ tests/test_get_oracle.py
@@ -1,12 +1,13 @@
 from ask_fandom.oracle.base import get_oracle
+from ask_fandom.oracle.oracles import PersonFactOracle, EpisodeFactOracle
 
 
 def test_get_oracle():
     assert get_oracle(question='When was Jake Simmonds born?') == \
-        ['PersonFactOracle', {'name': 'Jake Simmonds', 'property': 'born'}]
+        [PersonFactOracle.__name__, {'name': 'Jake Simmonds', 'property': 'born'}]
 
     assert get_oracle(question='Who directed The Big Bang episode?') == \
-        ['EpisodeFactOracle', {'name': 'The Big Bang episode', 'property': 'directed'}]
+        [EpisodeFactOracle.__name__, {'name': 'The Big Bang episode', 'property': 'directed'}]
 
     assert get_oracle(question='Who played in The End of Time episode?') == \
-        ['EpisodeFactOracle', {'name': 'The End of Time episode', 'property': 'played'}]
+        [EpisodeFactOracle.__name__, {'name': 'The End of Time episode', 'property': 'played'}]
