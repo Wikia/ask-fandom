9fdbe2a0433582be9268680c119bb12caa1b4406
macbre <maciej.brencz@gmail.com>
2019-03-15 11:28:39 +0100

    Add SpeechSynthesis - say the answer (see #3)

--- static/ask_fandom.js
+++ static/ask_fandom.js
@@ -1,5 +1,18 @@
 $(function() {
 
+    function say(text) {
+        if (window.speechSynthesis) {
+            // https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesisUtterance
+            var utterance = new SpeechSynthesisUtterance(text);
+            utterance.lang = 'en';
+
+            speechSynthesis.speak(utterance);
+        }
+        else {
+            console.error('speechSynthesis is not available');
+        }
+    }
+
     var form = $('form').eq(0),
         answer_wrapper = $('#answer-wrapper'),
         answer_node = $('#answer'),
@@ -11,6 +24,8 @@ $(function() {
         var question = $('#question').val();
         console.log('Asking the "' + question + '" question');
 
+        answer_wrapper.hide();
+
         // https://api.jquery.com/jquery.getjson/
         $.getJSON('/ask', {'q': question}).
             done(function(resp) {
@@ -18,6 +33,8 @@ $(function() {
 
                 answer_wrapper.show();
                 answer_node.hide().fadeIn().text(resp.answer);
+
+                say(resp.answer);
             }).
             fail(function(resp) {
                 console.log('Got an error', resp.responseJSON);
