a652f580ec1751911686c53b3f2b30006560f7c2
macbre <maciej.brencz@gmail.com>
2019-03-15 09:06:49 +0100

    test_ask_fandom: test cases as a dictionary

--- tests/test_get_answer.py
+++ tests/test_get_answer.py
@@ -5,36 +5,28 @@ from ask_fandom.errors import AnswerNotKnownError
 
 
 def test_ask_fandom():
-    assert str(ask_fandom('Who played Agella?')) == 'Agella is played by Suzanne Danielle.'
-    assert str(ask_fandom('Who played Alaya?')) == 'Alaya is played by Neve McIntosh.'
+    test_cases = {
+        # question: answer
+        'Who played Agella?': 'Agella is played by Suzanne Danielle.',
+        'Who played Alaya?': 'Alaya is played by Neve McIntosh.',
+        'Who directed The Big Bang episode?': '"The Big Bang episode" episode has been directed by Toby Haynes.',
+        'Who produced The Big Bang episode?': '"The Big Bang episode" episode has been produced by Peter Bennett.',
+        'Which faction does the Alterac belong to?':  'Alterac is a member of "Contested territory" faction.',
+        'Which government does Alterac belong to?': 'Alterac is a member of "Hereditary monarchy" government.',
 
-    assert str(ask_fandom('Who directed The Big Bang episode?')) == \
-        '"The Big Bang episode" episode has been directed by Toby Haynes.'
+        # football
+        'Which club Romelu Lukaku plays for?': 'Romelu Lukaku plays for Manchester United F.C. now.',
 
-    assert str(ask_fandom('Who produced The Big Bang episode?')) == \
-        '"The Big Bang episode" episode has been produced by Peter Bennett.'
+        # similar questions
+        'Which club Cristiano Ronaldo plays for?': 'Cristiano Ronaldo plays for Juventus F.C. now.',
+        'Where is Cristiano Ronaldo playing now?': 'Cristiano Ronaldo plays for Juventus F.C. now.',
+        'Where does Cristiano Ronaldo play?': 'Cristiano Ronaldo plays for Juventus F.C. now.',
+    }
 
-    assert str(ask_fandom('Which faction does the Alterac belong to?')) == \
-        'Alterac is a member of "Contested territory" faction.'
-
-    assert str(ask_fandom('Which government does Alterac belong to?')) == \
-        'Alterac is a member of "Hereditary monarchy" government.'
-
-
-def test_football():
-    assert str(ask_fandom('Which club Romelu Lukaku plays for?')) == \
-        'Romelu Lukaku plays for Manchester United F.C. now.'
-
-
-def test_football_same_question():
-    assert str(ask_fandom('Which club Cristiano Ronaldo plays for?')) == \
-        'Cristiano Ronaldo plays for Juventus F.C. now.'
-
-    assert str(ask_fandom('Where is Cristiano Ronaldo playing now?')) == \
-        'Cristiano Ronaldo plays for Juventus F.C. now.'
-
-    assert str(ask_fandom('Where does Cristiano Ronaldo play?')) == \
-        'Cristiano Ronaldo plays for Juventus F.C. now.'
+    for question, expected_answer in test_cases.items():
+        answer = ask_fandom(question)
+        print(answer)
+        assert str(answer) == expected_answer, question
 
 
 def test_ask_fandom_dont_know():
